c9242898e65b993b6a9ba89ca76f2fed8da23a11
NooberThanYall <loudervibes@gmail.com>
2024-12-03 14:30:06 +0330

    Initial Commit

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['pbs.twimg.com', 'assets.aceternity.com', 'images.unsplash.com','zeba.academy']
+    }
+};
+
+export default nextConfig;
