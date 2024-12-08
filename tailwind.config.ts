48fe4a461e984bb9f0241abc41bed9ae15db796d
NooberThanYall <loudervibes@gmail.com>
2024-12-08 07:20:37 +0330

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,10 +4,12 @@ const {
 } = require("tailwindcss/lib/util/flattenColorPalette");
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  "./app/**/*.{js,ts,jsx,tsx,mdx}",
+],
+
   theme: {
     extend: {
       colors: {
